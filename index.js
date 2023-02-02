b1b9d3fd48affc7fa0aaad59ee04bcb0e41d560d
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-02-01 22:26:04 -0500

    commit tree c8cc193f9f3f1436fed34575ffd68c5020ac6259

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names) {
+    let cards = []
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    }
+    return cards;
+}
+writeCards(names);
+
+function countDown() {
+
+let countDown = 10;
+
+while (countDown >= 0){
+    console.log(countDown--);
+    }
+}
